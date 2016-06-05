3db758f2fef41757cdafd18f353707050c5fc0fb
Ray Villalobos <ray@lynda.com>
2016-06-04 20:44:44 -0400

    02_02e

--- process/js/app.js
+++ process/js/app.js
@@ -2,11 +2,28 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var MainInterface = React.createClass({
+  getInitialState: function() {
+    return {
+      title: 'Appointments',
+      show: true
+    } //return
+  }, //getInitialState
+
   render: function() {
+    var showTitle;
+    if (this.state.show) {
+      showTitle = 'New';
+    }
+
+    var displayList = {
+      display: this.state.show ? 'block':'none',
+      color: 'red'
+    }
+
     return (
       <div className="interface">
-        <h1>Pet Appointments</h1>
-        <ul>
+        <h1>{ showTitle } {this.state.title}</h1>
+        <ul style={displayList}>
           <li>Buffy 3:30 PM</li>
           <li>Spot 8:30 PM</li>
           <li>Goldie 3:50 PM</li>
