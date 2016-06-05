121bfc9aa7fcedfbba880ba618f17e1081029deb
Ray Villalobos <ray@lynda.com>
2016-06-05 15:42:02 -0400

    02_04e

--- process/js/app.js
+++ process/js/app.js
@@ -4,48 +4,36 @@ var ReactDOM = require('react-dom');
 var MainInterface = React.createClass({
   getInitialState: function() {
     return {
-      data: [
-        {
-          "petName": "Buffy",
-          "ownerName": "Hassum Harrod",
-          "aptDate": "2016-06-20 15:30",
-          "aptNotes": "This Chihuahua has not eaten for three days and is lethargic"
-        },
-        {
-          "petName": "Spot",
-          "ownerName": "Constance Smith",
-          "aptDate": "2016-06-24 08:30",
-          "aptNotes": "This German Shepherd is having some back pain"
-        },
-        {
-          "petName": "Goldie",
-          "ownerName": "Barot Bellingham",
-          "aptDate": "2016-06-22 15:50",
-          "aptNotes": "This Goldfish has some weird spots in the belly"
-        },
-        {
-          "petName": "Mitten",
-          "ownerName": "Hillary Goldwyn",
-          "aptDate": "2016-06-21 9:15",
-          "aptNotes": "Cat has excessive hairballs"
-        }
-      ]
+      myAppointments: []
     } //return
   }, //getInitialState
 
+  componentDidMount: function() {
+    this.serverRequest = $.get('./js/data.json', function(result) {
+      var tempApts = result;
+      this.setState({
+        myAppointments: tempApts 
+      }); //setState
+    }.bind(this));
+  },
+
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+
   render: function() {
-    var filteredApts = this.state.data;
+    var filteredApts = this.state.myAppointments;
     filteredApts = filteredApts.map(function(item, index) {
       return(
         <li className="pet-item media" key={index}>
           <div className="pet-info media-body">
             <div className="pet-head">
-              <span className="pet-name">{this.state.data[index].petName}</span>
-              <span className="apt-date pull-right">{this.state.data[index].aptDate}</span>
+              <span className="pet-name">{this.state.myAppointments[index].petName}</span>
+              <span className="apt-date pull-right">{this.state.myAppointments[index].aptDate}</span>
             </div>
             <div className="owner-name"><span className="label-item">Owner:</span>
-            {this.state.data[index].ownerName}</div>
-            <div className="apt-notes">{this.state.data[index].aptNotes}</div>
+            {this.state.myAppointments[index].ownerName}</div>
+            <div className="apt-notes">{this.state.myAppointments[index].aptNotes}</div>
           </div>
         </li>
       ) //return
